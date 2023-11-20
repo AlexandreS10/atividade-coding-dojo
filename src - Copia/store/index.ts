df2f804a7d767e9acd563a3498edf3165018cd97
AlexandreS10 <alexandres10santos@gmail.com>
2023-11-20 18:29:10 -0300

    ajustes

--- src - Copia/store/index.ts
+++
@@ -1,4 +0,0 @@
-import { store, RootState, AppDispatch, persistor } from "./store";
-
-export { store, persistor };
-export type { RootState, AppDispatch };
--- src - Copia/store/modules/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import userSlice from "./users/userSlice";
-import usersSlice from "./users/usersSlice";
-
-export default combineReducers({
-  user: userSlice,
-  users: usersSlice,
-});
